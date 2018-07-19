9782af2da24beca469acbbbe7b3aa4b41daac2a1
tfausten <41119754+tfausten@users.noreply.github.com>
2018-07-19 21:59:01 +0200

    speed up calculations on country dispersion

--- scripts/TPF_country_dispersion_index.R
+++ scripts/TPF_country_dispersion_index.R
@@ -8,7 +8,7 @@ calc.disp <- function (x) {
   uni <- unique(x)
   sharevec <- vector(mode = "numeric", length = length(uni))
   
-  for (i in 1:length(sharevec)) {
+  for (i in 1:length(sharevec)) {             #for-loop actually computes faster than lapply here
     sharevec[i] <- sum(x == uni[i])/length(x)
   }
   return(1 - sum(sharevec^2))
@@ -25,12 +25,16 @@ str(allcountries_app)
 dispersion <- data.frame(family_id = 1:length(allcountries_app), D_app = NA, D_inv = NA)
 
 #use the function to calculate dispersion for all entries (and save to file for later use)
-dispersion$D_app <- lapply(allcountries_app, calc.disp2)
+dispersion$D_app <- sapply(allcountries_app, calc.disp)
 dispersion$D_inv <- sapply(allcountries_inv, calc.disp)
 save(dispersion, file = "./datasource/TPF/dispersion.Rdata")
 
-####
+#allcountries not needed anymore
+rm(allcountries_app, allcountries_inv)
+
+#how many dispersion entries are !=0 and not NaN (NaN values result from division by 0 if there is no country entry)
 length(which(dispersion$D_inv != 0 & is.nan(dispersion$D_inv) == FALSE))
+#how many are NaN?
 length(which(is.nan(dispersion$D_inv)))
 
 ##############
@@ -41,30 +45,38 @@ length(which(is.nan(dispersion$D_inv)))
 countrygroup <- c("JP", "US", "DE", "CN", "KR", "FR", "CH", "GB")
 load("./datasource/TPF/tpf_families.RData")
 #data.frame for yearly country dispersion data
-disp_data <- data.frame(year = rep(1975:2016, length.out = 42 * length(countrygroup)), 
-                        country = rep(countrygroup, each = 42), inv_disp = NA, app_disp = NA, 
+disp_data <- data.frame(year = rep(1975:2016, length.out = (2016 - 1974) * length(countrygroup)), 
+                        country = rep(countrygroup, each = (2016 - 1974)), inv_disp = NA, app_disp = NA, 
                         inv_count = NA, app_count = NA, stringsAsFactors = FALSE)
 
-#for loop to fill the data.frame. Create workingvec of type character to avoid repeated conversion to character vector in the loop through the
-#grep function
-workingvec_inv <- as.character(allcountries_inv)
-workingvec_app <- as.character(allcountries_app)
-for (i in 1975:2016) {
+#for loop to fill the data.frame. Create workingvec of type character to avoid repeated conversion to character 
+#vector in the loop through the grep function
+load("./datasource/TPF/unicountries_inv.Rdata")
+load("./datasource/TPF/unicountries_app.Rdata")
+unicountries_inv <- as.character(unicountries_inv)
+unicountries_app <- as.character(unicountries_app)
+
+
+whichyears <- sapply(1975:2016, function(x) {which(tpf_families$first_app_year == x)})
+names(whichyears) <- 1975:2016
+whichcountries_inv <- lapply(countrygroup, function(x) {grep(x, unicountries_inv, fixed = TRUE)})
+names(whichcountries_inv) <- countrygroup
+whichcountries_app <- lapply(countrygroup, function(x) {grep(x, unicountries_app, fixed = TRUE)})
+names(whichcountries_app) <- countrygroup
+
+for (y in countrygroup) {
   
-  years <- which(tpf_families$first_app_year == i)
-  
-  for (y in countrygroup) {
-    #inventor dispersioin
-    ctry <- grep(y, workingvec_inv, fixed = TRUE)
-    sub <- dispersion$D_inv[intersect(ctry, years)]
-    disp_data$inv_disp[disp_data$year == i & disp_data$country == y] <- mean(sub)
-    disp_data$inv_count[disp_data$year == i & disp_data$country == y] <- length(sub)
+  for (i in 1975:2016) {
+
+    #inventor dispersion
+    isect <- intersect('[['(whichyears, as.character(i)), '[['(whichcountries_inv, y))  
+    disp_data$inv_disp[disp_data$year == i & disp_data$country == y] <- mean(dispersion$D_inv[isect])
+    disp_data$inv_count[disp_data$year == i & disp_data$country == y] <- length(dispersion$D_inv[isect])
     
     #applicant dispersion
-    ctry <- grep(y, workingvec_app, fixed = TRUE)
-    sub <- dispersion$D_app[intersect(ctry, years)]
-    disp_data$app_disp[disp_data$year == i & disp_data$country == y] <- mean(sub)
-    disp_data$app_count[disp_data$year == i & disp_data$country == y] <- length(sub)
+    isect <- intersect('[['(whichyears, as.character(i)), '[['(whichcountries_app, y)) 
+    disp_data$app_disp[disp_data$year == i & disp_data$country == y] <- mean(dispersion$D_app[isect])
+    disp_data$app_count[disp_data$year == i & disp_data$country == y] <- length(dispersion$D_app[isect])
     
     cat(i, y, " ")
   }
