ce41ffedb8347e1cda0b436c78a5603f4b5f26c6
tfausten <41119754+tfausten@users.noreply.github.com>
2018-07-28 16:57:48 +0200

    ipc processing

--- scripts/TPF_co-classification.R
+++ scripts/TPF_co-classification.R
@@ -1,10 +1,10 @@
-##description
+##This script uses IPC classifications to make a co-classification matrix of tpf
 load("./datasource/TPF/201803_TPF_IPC.RData")
-
 str(tpf_ipc)
-head(tpf_ipc, 30)
+head(tpf_ipc)
+length(unique(tpf_ipc$IPC))
 
-#make small subset to tpf_ipc for experimentation
+#make small subsets of tpf_ipc for experimentation
 tpf_ipc23 <- tpf_ipc[1:23, ]
 tpf_ipc1000 <- tpf_ipc[1:1000, ]
 tpf_ipc10k <- tpf_ipc[1:10000, ]
@@ -17,45 +17,28 @@ str(tpf_ipc1000)
 
 ######
 #function that returns a co-classification matrix from input of tpf_ipc dataset. IPC variable is assumed to be of type factor
-coclass <- function (data) {
-  comatrix <- matrix(data = as.numeric(0), nrow = length(levels(data$IPC)), ncol = length(levels(data$IPC)))
-  rownames(comatrix) <- levels(data$IPC)
-  colnames(comatrix) <- levels(data$IPC)
-  
-  for (i in 1:max(data$Family_id)) {
-    datasub <- which(data$Family_id == i)
-    print(i)
-    
-    for (x in data[datasub, "IPC"]) {
-      for (y in data[datasub, "IPC"]) {
-        if (x == y) next #avoid 
-        comatrix[x, y] <- comatrix[x, y] + 1
-      }
-    }
-  }
-  
-  return(comatrix)
-}
 
-#alternative approach test
-coclass2 <- function (data) {
-  comatrix <- matrix(data = as.numeric(0), nrow = length(levels(data$IPC)), ncol = length(levels(data$IPC)))
-  rownames(comatrix) <- levels(data$IPC)
-  colnames(comatrix) <- levels(data$IPC)
+coclass <- function (data) {
+  ipc_classes <- levels(data$IPC)
+  comatrix <- matrix(data = as.numeric(0), nrow = length(ipc_classes), ncol = length(ipc_classes))
+  rownames(comatrix) <- ipc_classes
+  colnames(comatrix) <- ipc_classes
   
   for (i in 1:max(data$Family_id)) {
-    print(i)
     ipc_vec <- data[data$Family_id == i, "IPC"]
-    
-    #for (y in ipc_vec) {
-     # comatrix[ipc_vec, y] <- comatrix[ipc_vec, y] + 1
-    #}
+
     comatrix[ipc_vec, ipc_vec] <- comatrix[ipc_vec, ipc_vec] + 1
   }
+
   return(comatrix)
 }
 
-test <- coclass2(tpf_ipc10k)
+start_time <- Sys.time()
+test <- coclass(tpf_ipc10k)
+test <- coclass(tpf_ipc23)
+print(Sys.time() - start_time)
+
+
 str(test)
 max(test)
 isSymmetric(test)
