99a3eea01c93ff8d87b694f7a78dbb2ea50dcc5c
tfausten <41119754+tfausten@users.noreply.github.com>
2018-10-29 18:11:38 +0900

    scripting

--- scripts/TPF_knowledge-pools.R
+++ scripts/TPF_knowledge-pools.R
@@ -27,6 +27,28 @@ others3 <- function (data, year, limit, var = 'app') {
   others3 <- others3[- which(others3$country %in% del), ]
 }
 
-ann_counts_big <- others3(ann_counts, 2013, 200, 'app')
+ann_counts_big <- others3(ann_counts, 2013, 10, 'inv')
+totals2013 <- ann_counts_big[ann_counts_big$year == 2013 & ann_counts_big$country != "others", ]
+totals2013[order(-totals2013$count_inv), ]
 
+hist(ann_counts_big$count_inv)
 
+load("./datasource/TPF/disp_inv.RData")
+load("./datasource/TPF/disp_app.RData")
+
+#add a dispersion columns to the 'totals' dataframe.
+totals2013$inv_disp <- NA
+totals2013$inv_disp <- sapply(totals2013$country, function(x) {disp_inv[disp_inv$country == x & disp_inv$year == 2013, "dispersion"]})
+totals2013$app_disp <- sapply(totals2013$country, function(x) {disp_app[disp_app$country == x & disp_app$year == 2013, "dispersion"]})
+
+hist(log(totals2013$count_inv))
+plot(log(totals2013$count_inv), totals2013$inv_disp)
+abline(lm(totals2013$inv_disp ~ log(totals2013$count_inv)))
+text(log(totals2013$count_inv), totals2013$inv_disp, labels = totals2013$country, cex =0.7, pos = 3)
+summary(lm(totals2013$inv_disp ~ log(totals2013$count_inv)))
+
+hist(log(totals2013$count_app))
+plot(log(totals2013$count_app), totals2013$app_disp)
+abline(lm(totals2013$app_disp ~ log(totals2013$count_app)))
+text(log(totals2013$count_app), totals2013$app_disp, labels = totals2013$country, cex =0.7, pos = 3)
+summary(lm(totals2013$app_disp ~ log(totals2013$count_app)))
