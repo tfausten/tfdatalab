2a3c8c9785c71c1f4828a540e55152fa61e89fa4
tfausten <41119754+tfausten@users.noreply.github.com>
2018-11-09 17:15:13 +0900

    Update scripts/auto_regression.R

--- scripts/auto_regression.R
+++ scripts/auto_regression.R
@@ -1,24 +1,27 @@
 #learn about auto-regression in R, create some auto-correlated random variables
 
-#One way to create an auto-correlated random variable is the following
-plot(diffinv(rnorm(50)), type = 'l')
-#whereas
-plot(rnorm(50), type = 'l')
-#Why does this work?
+#Create a normally distributed variable and plot it
+set.seed(42)
+norm_y <- rnorm(50)
+plot(norm_y, type = "l")
 
+#One way to create an auto-correlated random variable is the following
+autocor_y <- diffinv(norm_y)
+plot(autocor_y, type = 'l')
+
+#Why does this work?
 #The R-documentation tells us that diffinv() is the reverse function of diff()
 #what does the diff() function do?
 #diff(x) (x being a vector of length n) computes the vector y of length n-1 with y_i = x_i+1 - x_i. In short the function computes successive differences
 diff(c(1:10))
 diff(c(10:1))
 
-
+.
 #If applied to a normally distributed random variable plotted, diff() results in a zig-zag shaped line. Simply said, this is because the mean of the normal distribution
 #(in this case 0) is always the most likely outcome of the random variable. Thus, a positive value is most likely to have a positive difference to the preceding random value.
 #The difference to the subsequent value will however be most likely negative. This is a case of auto-correlation with lag 1 and a negative correlation coefficient.
-cat(x <- rnorm(200))
-diff(x)
-plot(diff(x), type = 'l')
+neg_autocor_y <- diff(norm_y)
+plot(neg_autocor_y, type = 'l')
 
 
 #what does diffinv() do?
@@ -34,7 +37,18 @@ diffinv(diff(16:25), xi = 16)
 #Here diffinv treats the normally distributed random values as successive differences of a certain starting vector. The curve starts at y_1 = 0, as no starting value was
 #specified. Afterwards: y_i = y_i-1 - x_i-1. Thus, each y value functions as a base to which the next random value is added. In terms of auto-correlation,
 #the values are positively auto-correlated with lag = 1.
-x
-diffinv(x)
 
-plot(diffinv(x), type = 'l')
+##investigate the variable using autoregression analysis
+set.seed(42)
+norm_y <- rnorm(1000)
+autocor_y <- diffinv(norm_y)
+neg_autocor_y <- diff(norm_y)
+
+ar(autocor_y)
+ar(neg_autocor_y)
+
+acf(autocor_y)
+pacf(autocor_y)
+
+acf(neg_autocor_y)
+pacf(neg_autocor_y)
